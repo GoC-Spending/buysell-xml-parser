e0fc92147ea49753f567cdd2765d5ce82f4b5f4a
Lucas Cherkewski <lucas@ecustom.ca>
2017-06-23 11:06:14 -0400

    functional keys/values

--- parser.php
+++ parser.php
@@ -8,14 +8,37 @@ $data = file_get_contents(dirname(__FILE__) . '/data.xml');
 
 $xs = Selector::loadXML($data);
 
-$contracts = $xs->findAll('//item');
-
-foreach ($contracts as $contract) {
-    $contractValues = [
+$contracts = $xs->findAll('//item')->map(function($contract) {
+    return [
         'title' => $contract->find('title')->extract(),
         'url' => $contract->find('link')->extract(),
         'department' => $contract->find('dc:creator')->extract(),
+        'description' => parseDescription($contract->find('description')->extract()),
     ];
+});
 
-    var_dump($contractValues);
+var_dump($contracts);
+
+function parseDescription($description) {
+    $descriptionKeys = [];
+    $descriptionValues = [];
+
+    $xs = Selector::loadHTML($description);
+
+    $descriptionRows = $xs->findAll('//td');
+
+
+    // There's no signifier of heading vs value, so we walk the tree to find keys and values
+    $i = 0;
+    foreach ($descriptionRows as $row) {
+        if ($i % 2 == 0) {
+            $descriptionKeys[] = $row->extract();
+        } else {
+            $descriptionValues[] = $row->extract();
+        }
+
+        $i++;
+    }
+
+    return array_combine($descriptionKeys, $descriptionValues);;
 }
