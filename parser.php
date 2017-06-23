81a50c3a1353269088a1c8c9a6b4750064cbac4f
Lucas Cherkewski <lucas@ecustom.ca>
2017-06-23 10:49:39 -0400

    basic parsing

--- parser.php
+++ parser.php
@@ -8,4 +8,14 @@ $data = file_get_contents(dirname(__FILE__) . '/data.xml');
 
 $xs = Selector::loadXML($data);
 
+$contracts = $xs->findAll('//item');
 
+foreach ($contracts as $contract) {
+    $contractValues = [
+        'title' => $contract->find('title')->extract(),
+        'url' => $contract->find('link')->extract(),
+        'department' => $contract->find('dc:creator')->extract(),
+    ];
+
+    var_dump($contractValues);
+}
